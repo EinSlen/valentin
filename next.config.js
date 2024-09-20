a66ee74579d1ec031c35c1ce4e1edc4e283f0142
DVLaD <valentin.damlencour@gmail.com>
2024-09-20 19:56:46 +0200

    add next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'out',
+    experimental: {
+        optimizePackageImports: ['icon-library'],
+        optimizeCss: true,
+        optimisticClientCache: true,
+        optimizeServerReact: true
+    },
+    swcMinify: true,
   i18n: {
     locales: ['fr', 'en'],
     defaultLocale: 'fr'
