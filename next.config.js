f56a89f332e673c6ef3c7dfef1e6aa9b3229211e
DVLaD <valentin.damlencour@gmail.com>
2025-08-17 19:32:38 +0200

    fix content config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    distDir: 'build',
+    distDir: "build",
     experimental: {
-        optimizePackageImports: ['icon-library'],
+        optimizePackageImports: ["icon-library"],
         optimizeCss: true,
         optimisticClientCache: true,
-        optimizeServerReact: true
+        optimizeServerReact: true,
     },
     swcMinify: true,
-  i18n: {
-    locales: ['fr', 'en'],
-    defaultLocale: 'fr'
-  }
-}
-
-module.exports = nextConfig
-
-module.exports = {
+    i18n: {
+        locales: ["fr", "en"],
+        defaultLocale: "fr",
+    },
     images: {
-      domains: ['avatars.githubusercontent.com'],
+        domains: ["avatars.githubusercontent.com"],
     },
 };
+
+module.exports = nextConfig;
